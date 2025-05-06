db2c17d96838fd7651325ee203f70e1b4651405d
Duncan Calvert <dcalvert@ihmc.org>
2025-05-06 12:32:00 -0500

    Add load and compute stats.

---
+++ lerobot/common/datasets/load_and_compute_stats.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
+
+def main(dataset_path: str) -> None:
+    dataset_path = Path(dataset_path)
+
+    print(f"Dataset path: {dataset_path}")
+
+    dataset = LeRobotDataset(f'root/{dataset_path.name}', dataset_path, tolerance_s=0.1)
+    dataset.load_hf_dataset()
+    dataset.episode_buffer = dataset.create_episode_buffer()
+
+    for frame_index in range(dataset.num_frames):
+
+        frame = {
+            "action": [0, 1],
+            "timestamp": 0.1,
+        }
+
+        dataset.add_frame(frame)
+
+
+    dataset.save_episode()
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <dataset_path>")
+        sys.exit(1)
+    main(sys.argv[1])
