0a34f02604249115f3280a35c55b548a59255648
Duncan Calvert <dcalvert@ihmc.org>
2025-05-21 18:51:12 -0500

    Fix memory.

--- docker/lerobot-ihmc/runTemporaryBash.sh
+++ docker/lerobot-ihmc/runTemporaryBash.sh
@@ -12,6 +12,7 @@ docker run \
     --env "TERM=xterm-256color" `# Enable color in the terminal` \
     --privileged \
     --gpus all \
+    --shm-size=20g \
     --volume /home/$USER/lerobot:/lerobot \
     --volume /home/$USER/datasets:/datasets \
     ihmcrobotics/lerobot-ihmc:0.1 bash
