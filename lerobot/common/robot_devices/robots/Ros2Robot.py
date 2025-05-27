faf5c2852c7165a4223d223f6997d15d5de5955e
Ben Pratt <c.ben.pratt@gmail.com>
2025-05-27 15:26:21 -0500

    Smple publisher with needed functions

--- lerobot/common/robot_devices/robots/Ros2Robot.py
+++ lerobot/common/robot_devices/robots/Ros2Robot.py
@@ -1,29 +1,49 @@
+
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
 
 class Ros2Robot(Node):
 
     def __init__(self):
-        super().__init__('Ros2Robot')
-        self.publisher_ = self.create_publisher(String, 'robot_output', 10)
-        timer_period = 0.5
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+
     def timer_callback(self):
         msg = String()
-        msg.data = self.robot_type
+        msg.data = 'Robot: %d' % self.i
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' %msg.data)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
+
     def connect(self):
         pass
     def run_calibration(self):
         pass
-    def teleop_step(self, record_data=False):
+
+    def teleop_step(self, record_data: bool = False):
         pass
-    def send_action(self, action):
+
+    def send_action(self, action_msg: str):
         pass
+
     def disconnect(self):
         pass
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = Ros2Robot()
+
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
