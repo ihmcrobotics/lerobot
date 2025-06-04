215714937f5057826a739df2147f06199f43061d
Ben Pratt <c.ben.pratt@gmail.com>
2025-06-04 14:07:47 -0500

    Clean up config

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -681,49 +681,39 @@ from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 @RobotConfig.register_subclass("ros2robot")
 @dataclass
 class Ros2RobotConfig(ManipulatorRobotConfig):
-    # ROS topics
     subscribers: dict[str, tuple[type, str, QoSProfile]] = field(
         default_factory=lambda: {
             '/lerobot/zed/left/color': (
-                Image,
-                '_left_color_callback',
+                Image, '_left_color_callback',
                 QoSProfile(depth=10, reliability=QoSReliabilityPolicy.BEST_EFFORT)
             ),
             '/lerobot/zed/right/color': (
-                Image,
-                '_right_color_callback',
+                Image, '_right_color_callback',
                 QoSProfile(depth=10, reliability=QoSReliabilityPolicy.BEST_EFFORT)
             ),
             '/lerobot/lerobot/state/hand_poses': (
-                Float32MultiArray,
-                '_state_hand_poses_callback',
+                Float32MultiArray, '_state_hand_poses_callback',
                 QoSProfile(depth=10, reliability=QoSReliabilityPolicy.BEST_EFFORT)
             ),
             '/lerobot/connect': (
-                String,
-                '_connect_callback',
+                String, '_connect_callback',
                 QoSProfile(depth=10, reliability=QoSReliabilityPolicy.BEST_EFFORT)
             ),
             '/lerobot/command': (
-                String,
-                '_command_callback',
+                String, '_command_callback',
                 QoSProfile(depth=10, reliability=QoSReliabilityPolicy.BEST_EFFORT)
             ),
             '/lerobot/status': (
-                String,
-                '_status_callback',
+                String, '_status_callback',
                 QoSProfile(depth=10, reliability=QoSReliabilityPolicy.BEST_EFFORT)
             ),
         }
     )
     publishers: dict[str, tuple[type, int]] = field(
         default_factory=lambda: {
-            # I believe these are correct
             '/lerobot/lerobot/action/hand_poses': (Float32MultiArray, 10),
             '/lerobot/status': (String, 10),
         }
     )
-    # Control loop
     control_frequency: float   = 20.0  # Hz
-    # Optional limits & modes
     mock: bool = False
